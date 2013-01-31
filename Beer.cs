8849705eb7d1c7a32e69b0e244f89d2f41f33298
brock <brockjms1@gmail.com>
2013-01-31 14:44:59 -0500

    added beer.cs

--- Beer.cs
+++ Beer.cs
@@ -2,26 +2,28 @@ public class Beer
 {
 	public static void Main()
 	{
-		string Firts, Middle, Last;
+		string First, Middle, Last;
 
-		First = System.Console.ReadLine ("What is your first name: ");
-		Middle = System.Console.ReadLine ("What is your middle name: ");
-		Last = System.Console.Readline ("What is your last name: ");
-
+		System.Console.WriteLine ("What is your first name: ");
+		First = System.Console.ReadLine ();
+		System.Console.ReadLine ("What is your middle name: ");
+		Middle = System.Console.ReadLine();
+		System.Console.Readline ("What is your last name: ");
+		Last = System.Console.ReadLine();
 		System.Console.WriteLine (" Now I will take you name and print it backwards.");
 	
 		Drunk(Firts, Middle, Last);
 	}
 
-	public class void Drunk(string f, m, l)
+	public static void Drunk(string f, string m, string l)
 	{
-		string [] arr1 {f, m, l};
+		string []  arr1 =  {f, m, l};
 
 		int x;
 
                 for (x = arr1.Length - 1; x >= 0; x--)
                 {
-                        System.Console.WriteLine ("here you go " + arr[x]);
+                        System.Console.WriteLine ("here you go " + arr1[x]);
                 }
 	}
 }
