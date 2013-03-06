4511edad96852859d5b73ae792a883b729f06e13
brockjms1 <brockjms1@gmail.com>
2013-03-06 14:30:18 -0500

    getfiles change

--- getFiles.cs
+++ getFiles.cs
@@ -2,7 +2,8 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Collections.Generic.IEnumerable;
 
 public class getFiles
 {
@@ -14,7 +15,7 @@ public class getFiles
 		string ext1 = System.Console.ReadLine();
 		string[] ext2 = ext1.Split(',');
 
-		SortedDictionary<int, string> map = SortedDictionary<int, string>;
+		SortedDictionary<int, string> map = new SortedDictionary<int, string>();
 		
 		
 		foreach (string e in ext2)
@@ -22,15 +23,14 @@ public class getFiles
 			foreach( string f in Directory.GetFiles(input, e) )
 			{
 				FileInfo f1 = new FileInfo (f);
-				map.Add(ConvertToInt32(F1.Length), f);	
+				map.Add(Convert.ToInt32(f1.Length), f);	
 			}
 	
 		}
-		
 
 		foreach (var a in map)
 		{
-			Console.WriteLine("{0} is {1} bytes", a.Key, a.Value);
+			Console.WriteLine("{1} \tis {0} bytes", a.Key, a.Value);
 		}
 	}
 }
