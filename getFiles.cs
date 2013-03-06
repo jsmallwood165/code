3e4da71f20d2912e5cf02ff8a128a46f329347bb
brockjms1 <brockjms1@gmail.com>
2013-03-06 13:14:58 -0500

    first run at attempting get file by size

--- getFiles.cs
+++ getFiles.cs
@@ -1,4 +1,8 @@
+using System;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class getFiles
 {
@@ -9,16 +13,25 @@ public class getFiles
 		System.Console.WriteLine("What file extension would you like to see: ");
 		string ext1 = System.Console.ReadLine();
 		string[] ext2 = ext1.Split(',');
+
+		SortedDictionary<int, string> map = SortedDictionary<int, string>;
 		
 		
 		foreach (string e in ext2)
 		{
 			foreach( string f in Directory.GetFiles(input, e) )
 			{
-				System.Console.WriteLine("> " + f);
+				FileInfo f1 = new FileInfo (f);
+				map.Add(ConvertToInt32(F1.Length), f);	
 			}
+	
 		}
+		
 
+		foreach (var a in map)
+		{
+			Console.WriteLine("{0} is {1} bytes", a.Key, a.Value);
+		}
 	}
 }
 
